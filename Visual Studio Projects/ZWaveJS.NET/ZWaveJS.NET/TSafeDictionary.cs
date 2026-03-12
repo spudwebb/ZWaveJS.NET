211b35a3cd6633c470daeeb8e493ff813d61e4d9
spud <spud.webb.4@gmail.com>
2026-03-12 13:31:23 -0400

    Use a thread safe dictionary for callbacks.

---
+++ Visual Studio Projects/ZWaveJS.NET/ZWaveJS.NET/TSafeDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZWaveJS.NET
+{
+    /// <summary>
+    /// Thread-safe Dictionary
+    /// </summary>
+    /// <remarks>
+    /// ConcurrentDictionary wrapper class exposing a partial 
+    /// "Dictionary-like" interface for use in this application.
+    /// </remarks>
+    internal class TSafeDictionary<K, V>
+    {
+
+        private ConcurrentDictionary<K, V> _dictionary = new ConcurrentDictionary<K, V>();
+
+        public V this[K key]
+        {
+            get
+            {
+                if (!_dictionary.TryGetValue(key, out V value))
+                {
+                    throw new KeyNotFoundException();
+                }
+                return value;
+            }
+            private set { }
+        }
+
+        public ICollection<K> Keys
+        {
+            get { return (_dictionary.Keys); }
+        }
+
+        public void Add(K key, V callback)
+        {
+            if (!_dictionary.TryAdd(key, callback))
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public bool Remove(K key)
+        {
+            return _dictionary.TryRemove(key, out _);
+        }
+
+        public bool ContainsKey(K key)
+        {
+            return _dictionary.ContainsKey(key);
+        }
+    }
+}
